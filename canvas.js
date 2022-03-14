cc12ec2f9b10f93b32118dd6a8aeec6ec5bff074
belausabaev <b.usabaev@khm.de>
2022-03-14 22:32:15 +0100

    master volume and compressor

--- canvas.js
+++ canvas.js
@@ -84,8 +84,25 @@ let therSampler = false;
 let grainPlaying = false;
 
 
+let masterVol = 1;
 
+const masterVolume = new Tone.Volume(masterVol);
+const masterCompressor = new Tone.Compressor({
+  ratio: 12,
+  threshold: -28,
+  release: 0.25,
+  attack: 0.003,
+  knee: 30,
+});
+const masterAnalyser = new Tone.Analyser("waveform", 64);
 
+const gain1 = new Tone.Gain(0.1);
+const gain2 = new Tone.Gain(0.1);
+
+//todo
+//vibrato.frequency.value = "y value" * 10;
+
+Tone.Destination.chain(masterCompressor, masterVolume, masterAnalyser);
 
 /*
 function preload() {
@@ -216,6 +233,7 @@ function draw() {
                     // ok, vielleicht kann man hier probieren ein längeres sauberes theremin sample zu bekommen, und dann 
                     // mit asynchroner granular synthese etwas zu machen? dann klingt das ähnlich vom timbre des samples
                     // ist aber verbundener, und eher wie ein flächiger theremin sound
+
                     // hier envelopes hinzufügen, und die länge der Noten regeln
                     toNote(frequency);
                     console.log("cur note " + note);
@@ -227,7 +245,6 @@ function draw() {
                         } 
                       //else if same as before continue playin note??
                     } else if (lastNote == 0) {
-                        console.log("last note ist null");
                         sampler.triggerAttack(note, Tone.now());
                         lastNote = note;
 
